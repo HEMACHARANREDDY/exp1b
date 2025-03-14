91fadee886316ae4ab304f9e4ab500e02b634e2d
HEMACHARANREDDY <2310030419@klh.edu.in>
2025-03-14 13:27:58 +0530

    first commit

---
+++ exp1b/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+import React from "react";
+
+
+const GridDisplay = () => {
+  return (
+    <div className="container">
+      <div className="header">HEADER</div>
+      <div className="small-box1">SMALL BOX 1</div>
+      <div className="small-box2">SMALL BOX 2</div>
+      <div className="small-box3">SMALL BOX 3</div>
+      <div className="sidebar">SIDE BAR</div>
+      <div className="main-content">MAIN CONTENT</div>
+      <div className="footer">FOOTER</div>
+    </div>
+  );
+};
+
+export default GridDisplay;
